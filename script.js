895d567de3c32d8fa226ca7eba90f51ba3eb4bf4
JOSINALDO MOURA DOS SANTOS <josinaldo.msantos@senacsp.edu.br>
2023-10-06 11:24:32 -0300

    Click

---
+++ script.js
@@ -0,0 +1,15 @@
+function clickMe(){
+    document.querySelector('.click').innerText = "Clicou"
+}
+
+function passouMe(){
+    document.querySelector('.click').innerText = "Passou o mouse"
+}
+
+function tirouMe(){
+    document.querySelector('.click').innerText = "Tirou o mouse"
+}
+
+
+
+
